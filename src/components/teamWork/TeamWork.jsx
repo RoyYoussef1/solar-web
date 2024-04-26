c565b6bf20747a2211a1fa2413c123f27449b73b
Roy Youssef <royyoussef2002@hotmail.com>
2024-04-27 02:43:42 +0300

    about us page changes

--- src/components/teamWork/TeamWork.jsx
+++ src/components/teamWork/TeamWork.jsx
@@ -8,42 +8,49 @@ import icon4 from "../../assets/work-icon/icon-4.jpg";
 
 const TeamWork = () => {
   return (
-    <div className="zabre">
-      <div className="work-step-wrapper">
-        <div className="work-step-container">
-          <WorkStep
-            icon={icon1}
-            index={1}
-            title={"Meet Consultant"}
-            text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
-          />
-        </div>
-        <div className="work-step-container">
-          <WorkStep
-            icon={icon2}
-            index={2}
-            title={"Generate Power"}
-            text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
-          />
-        </div>
-        <div className="work-step-container">
-          <WorkStep
-            icon={icon3}
-            index={3}
-            title={"Save the Energy"}
-            text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
-          />
-        </div>
-        <div className="work-step-container">
-          <WorkStep
-            icon={icon4}
-            index={4}
-            title={"Use the Power"}
-            text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
-          />
+    <>
+      <div className="yellow-banner">
+        <div className="yellow-banner-text">
+            How Our Solar Team Works
         </div>
       </div>
-    </div>
+      <div className="steps-section">
+        <div className="work-step-wrapper">
+          <div className="work-step-container">
+            <WorkStep
+              icon={icon1}
+              index={1}
+              title={"Meet Consultant"}
+              text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
+            />
+          </div>
+          <div className="work-step-container">
+            <WorkStep
+              icon={icon2}
+              index={2}
+              title={"Generate Power"}
+              text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
+            />
+          </div>
+          <div className="work-step-container">
+            <WorkStep
+              icon={icon3}
+              index={3}
+              title={"Save the Energy"}
+              text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
+            />
+          </div>
+          <div className="work-step-container">
+            <WorkStep
+              icon={icon4}
+              index={4}
+              title={"Use the Power"}
+              text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
